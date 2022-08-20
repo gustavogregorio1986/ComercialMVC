63d74175a8347cccae8e08182f4f5ada5116b944
guga56 <gustavogregorio086@gmail.com>
2022-08-20 13:31:32 -0300

    criar o generic repositorio

---
+++ ComercialMVC.Data/Repositorio/GenericRepositorio.cs
@@ -0,0 +1,73 @@
+﻿using ComercialMVC.Data.Contexto;
+using ComercialMVC.Data.Repositorio.Interface;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComercialMVC.Data.Repositorio
+{
+    internal class GenericRepositorio<T> : IGenericRepositorio<T>, IDisposable where T : class
+    {
+        private readonly ComercialMVCContexto _db;
+
+        public void Adicionar(T entidade)
+        {
+            _db.Set<T>().Add(entidade);
+            Commit();
+        }
+
+        
+
+        public void Atualizar(T entidade)
+        {
+            _db.Entry(entidade).State = EntityState.Modified;
+            Commit();
+        }
+
+        public T Buscar(params object[] Key)
+        {
+            return _db.Set<T>().Find(Key);
+        }
+
+        public void Commit()
+        {
+            _db.SaveChanges();
+        }
+
+        public void Deletar(int i)
+        {
+            var obj = Buscar(i);
+            _db.Set<T>().Remove(obj);
+            Commit();
+        }
+
+        public void Dispose()
+        {
+            if (_db != null)
+            {
+                _db.Dispose();
+            }
+
+            GC.SuppressFinalize(this);
+        }
+
+        public List<T> Obter(Expression<Func<T, bool>> predicate)
+        {
+            return _db.Set<T>().Where(predicate).ToList();
+        }
+
+        public List<T> ObterTodos()
+        {
+            return _db.Set<T>().ToList();
+        }
+
+        public T Primeiro(Expression<Func<T, bool>> predicate)
+        {
+            return _db.Set<T>().Where(predicate).FirstOrDefault();
+        }
+    }
+}
---
+++ ComercialMVC.Data/Repositorio/Interface/IUsuarioRepositorio.cs
@@ -0,0 +1,13 @@
+﻿using ComercialMVC.Dominio.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComercialMVC.Data.Repositorio.Interface
+{
+    public interface IUsuarioRepositorio : IGenericRepositorio<Usuario>
+    {
+    }
+}
