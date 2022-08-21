1a1d251e1078991a38d8d6eef6597f3013b2a21d
guga56 <gustavogregorio086@gmail.com>
2022-08-21 09:28:48 -0300

    criar o menu e os links da pagina do menu

---
+++ ComercialMVC/Controllers/CadastroController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ComercialMVC.Controllers
+{
+    public class CadastroController : Controller
+    {
+        public IActionResult Cadastrar()
+        {
+            return View();
+        }
+    }
+}
---
+++ ComercialMVC/Controllers/LogarController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ComercialMVC.Controllers
+{
+    public class LogarController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
