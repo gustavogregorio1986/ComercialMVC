c55f69a076f0c9e7dd0acc5b0b696b4ba6a5631b
guga56 <gustavogregorio086@gmail.com>
2022-08-22 22:13:26 -0300

    criar um emtodo cadastrarlogin

--- ComercialMVC/Controllers/CadastroController.cs
+++ ComercialMVC/Controllers/CadastroController.cs
@@ -1,12 +1,34 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ComercialMVC.Dominio.Dominio;
+using ComercialMVC.Models;
+using ComercialMVC.Servico.Servico;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ComercialMVC.Controllers
 {
     public class CadastroController : Controller
     {
+        private readonly LoginServico loginServico;
+
+        public CadastroController()
+        {
+            loginServico = new LoginServico();
+        }
+
         public IActionResult Cadastrar()
         {
             return View();
+        }
+
+        public IActionResult CadastrarLogin(LoginView loginView)
+        {
+            loginView = new LoginView();
+            Login login = new Login();
+            loginView.IdLogin = login.IdLogin;
+            loginView.Email = login.Email;
+            loginView.Senha = login.Senha;
+            loginView.Perfil = login.Perfil;
+            loginServico.InserirLogin(login);
+            return View("Cadastrar");
         }
     }
 }
